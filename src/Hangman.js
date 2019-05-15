ce01d2483d5ba7e474312be530a922de527348eb
Parco Tam <ptam1556@gmail.com>
2019-05-15 13:25:04 -0700

    completed exercise

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,72 +1,72 @@
-import React, { Component } from "react";
-import "./Hangman.css";
-import img0 from "./0.jpg";
-import img1 from "./1.jpg";
-import img2 from "./2.jpg";
-import img3 from "./3.jpg";
-import img4 from "./4.jpg";
-import img5 from "./5.jpg";
-import img6 from "./6.jpg";
+import React, { Component } from 'react';
+import './Hangman.css';
+import img0 from './0.jpg';
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+import img6 from './6.jpg';
+import { randomWord } from './words';
 
 class Hangman extends Component {
-  /** by default, allow 6 guesses and use provided gallows images. */
-  static defaultProps = {
-    maxWrong: 6,
-    images: [img0, img1, img2, img3, img4, img5, img6]
-  };
+	/** by default, allow 6 guesses and use provided gallows images. */
+	static defaultProps = {
+		maxWrong: 6,
+		images: [ img0, img1, img2, img3, img4, img5, img6 ]
+	};
 
-  constructor(props) {
-    super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
-    this.handleGuess = this.handleGuess.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
+		this.handleGuess = this.handleGuess.bind(this);
+		this.handleRestart = this.handleRestart.bind(this);
+	}
 
-  /** guessedWord: show current-state of word:
+	/** guessedWord: show current-state of word:
     if guessed letters are {a,p,e}, show "app_e" for "apple"
   */
-  guessedWord() {
-    return this.state.answer
-      .split("")
-      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
-  }
+	guessedWord() {
+		return this.state.answer.split('').map((ltr) => (this.state.guessed.has(ltr) ? ltr : '_'));
+	}
 
-  /** handleGuest: handle a guessed letter:
+	/** handleGuest: handle a guessed letter:
     - add to guessed letters
     - if not in answer, increase number-wrong guesses
   */
-  handleGuess(evt) {
-    let ltr = evt.target.value;
-    this.setState(st => ({
-      guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
-    }));
-  }
+	handleGuess(evt) {
+		let ltr = evt.target.value;
+		this.setState((st) => ({
+			guessed: st.guessed.add(ltr),
+			nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+		}));
+	}
 
-  /** generateButtons: return array of letter buttons to render */
-  generateButtons() {
-    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
-      <button
-        key={ltr}
-        value={ltr}
-        onClick={this.handleGuess}
-        disabled={this.state.guessed.has(ltr)}
-      >
-        {ltr}
-      </button>
-    ));
-  }
+	/** generateButtons: return array of letter buttons to render */
+	generateButtons() {
+		return 'abcdefghijklmnopqrstuvwxyz'.split('').map((ltr) => (
+			<button className="Hangman-ltr-btn" key={ltr} value={ltr} onClick={this.handleGuess} disabled={this.state.guessed.has(ltr)}>
+				{ltr}
+			</button>
+		));
+	}
 
-  /** render: render game */
-  render() {
-    return (
-      <div className="Hangman">
-      <p>Wrong guesses: {this.state.nWrong}</p>
-        <img src={this.props.images[this.state.nWrong]} />
-        <p className="Hangman-word">{this.guessedWord()}</p>
-        <p>{this.generateButtons()}</p>
-      </div>
-    );
-  }
+	handleRestart() {
+		this.setState({ nWrong: 0, guessed: new Set(), answer: randomWord() });
+	}
+
+	/** render: render game */
+	render() {
+		return (
+			<div className="Hangman">
+				<p>Wrong guesses: {this.state.nWrong}</p>
+				<img src={this.props.images[this.state.nWrong]} />
+				<p className="Hangman-word">{this.guessedWord()}</p>
+        <p>{ this.state.nWrong < this.props.maxWrong ? this.generateButtons() : null }</p>
+				<button className="Hangman-reset" onClick={this.handleRestart}> Restart </button>
+			</div>
+		);
+	}
 }
 
 export default Hangman;
--- src/__tests__/hangman.test.js
+++ src/__tests__/hangman.test.js
@@ -1,115 +1,86 @@
-	
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from "enzyme-to-json";
+import toJson from 'enzyme-to-json';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow, mount } from "enzyme";
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import Hangman from '../Hangman';
 
-import img0 from '../../src/0.jpg'
-import img1 from '../../src/1.jpg'
+import img0 from '../../src/0.jpg';
+import img1 from '../../src/1.jpg';
 
 configure({ adapter: new Adapter() });
 
-it("App renders without crashing", function() {
-  shallow(<App />);
+it('App renders without crashing', function() {
+	shallow(<App />);
 });
 
 it('Hangman Component renders without errors', function() {
-  const div = document.createElement('div');
-  ReactDOM.render(<Hangman />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<Hangman />, div);
+	ReactDOM.unmountComponentAtNode(div);
 });
 
-it("matches snapshot", function() {
-  let wrapper = shallow(<Hangman />);
-  let serialized = toJson(wrapper);
-  expect(serialized).toMatchSnapshot();
+it('matches snapshot', function() {
+	let wrapper = shallow(<Hangman />);
+	let serialized = toJson(wrapper);
+	expect(serialized).toMatchSnapshot();
 });
 
 it('it should should not change image on correct guess', function() {
   let wrapper = mount(<Hangman />);
-  wrapper
-    .find("button[value='a']")
-    .simulate('click', {target: { value: "a" }})
+  wrapper.setState({ 'answer': 'apple' })
+	wrapper.find("button[value='a']").simulate('click', { target: { value: 'a' } });
 
-  expect(wrapper.html()).toContain(img0)
+	expect(wrapper.html()).toContain(img0);
 });
 
 it('it should render img1 after incorrect guess', function() {
-  let wrapper = mount(<Hangman />);
-  wrapper
-    .find("button[value='z']")
-    .simulate('click', {target: { value: "z" }})
+	let wrapper = mount(<Hangman />);
+	wrapper.setState({ answer: 'apple' });
+	wrapper.find("button[value='z']").simulate('click', { target: { value: 'z' } });
 
-  expect(wrapper.html()).toContain(img1)
+	expect(wrapper.html()).toContain(img1);
 });
 
-
 it('it should update number of wrong guesses', function() {
-  let wrapper = mount(<Hangman />);
-  wrapper
-    .find("button[value='z']")
-    .simulate('click', {target: { value: "z" }})
+	let wrapper = mount(<Hangman />);
+	wrapper.setState({ answer: 'apple' });
+	wrapper.find("button[value='z']").simulate('click', { target: { value: 'z' } });
 
-  expect(wrapper.state('nWrong')).toEqual(1);
-  // expect(wrapper.exists('a')).toEqual(true);
-  // expect(wrapper.html()).toContain("Think of a Question");
+	expect(wrapper.state('nWrong')).toEqual(1);
+	// expect(wrapper.exists('a')).toEqual(true);
+	// expect(wrapper.html()).toContain("Think of a Question");
 });
 
 it('it should contain an image after maxGuesses', function() {
-  let wrapper = mount(<Hangman />);
-  // simulate maxGuesses
-  wrapper
-    .find("button[value='z']")
-    .simulate('click', {target: { value: "z" }})
-  wrapper
-    .find("button[value='y']")
-    .simulate('click', {target: { value: "y" }})
-  wrapper
-    .find("button[value='w']")
-    .simulate('click', {target: { value: "w" }})
-  wrapper
-    .find("button[value='v']")
-    .simulate('click', {target: { value: "v" }})
-  wrapper
-    .find("button[value='t']")
-    .simulate('click', {target: { value: "t" }})
-  wrapper
-    .find("button[value='u']")
-    .simulate('click', {target: { value: "u" }})
-  wrapper
-    .find("button[value='b']")
-    .simulate('click', {target: { value: "b" }})
+	let wrapper = mount(<Hangman />);
+	wrapper.setState({ answer: 'apple' });
+	// simulate maxGuesses
+	wrapper.find("button[value='z']").simulate('click', { target: { value: 'z' } });
+	wrapper.find("button[value='y']").simulate('click', { target: { value: 'y' } });
+	wrapper.find("button[value='w']").simulate('click', { target: { value: 'w' } });
+	wrapper.find("button[value='v']").simulate('click', { target: { value: 'v' } });
+	wrapper.find("button[value='t']").simulate('click', { target: { value: 't' } });
+	wrapper.find("button[value='u']").simulate('click', { target: { value: 'u' } });
 
-  expect(wrapper.html()).toContain('.jpg')
+	expect(wrapper.html()).toContain('.jpg');
 });
 
 it('it should not allow clicks after maxGuesses', function() {
-  let wrapper = mount(<Hangman />);
-  // simulate maxGuesses
-  wrapper
-    .find("button[value='z']")
-    .simulate('click', {target: { value: "z" }})
-  wrapper
-    .find("button[value='y']")
-    .simulate('click', {target: { value: "y" }})
-  wrapper
-    .find("button[value='w']")
-    .simulate('click', {target: { value: "w" }})
-  wrapper
-    .find("button[value='v']")
-    .simulate('click', {target: { value: "v" }})
-  wrapper
-    .find("button[value='t']")
-    .simulate('click', {target: { value: "t" }})
-wrapper
-    .find("button[value='u']")
-    .simulate('click', {target: { value: "u" }})
+	let wrapper = mount(<Hangman />);
+	wrapper.setState({ answer: 'apple' });
+	// simulate maxGuesses
+	wrapper.find("button[value='z']").simulate('click', { target: { value: 'z' } });
+	wrapper.find("button[value='y']").simulate('click', { target: { value: 'y' } });
+	wrapper.find("button[value='w']").simulate('click', { target: { value: 'w' } });
+	wrapper.find("button[value='v']").simulate('click', { target: { value: 'v' } });
+	wrapper.find("button[value='t']").simulate('click', { target: { value: 't' } });
+	wrapper.find("button[value='u']").simulate('click', { target: { value: 'u' } });
 
-  expect(wrapper.html()).not.toContain('button')
+  // searches for className
+	expect(wrapper.html()).not.toContain('Hangman-ltr-btn');
 });
 
 // it('renders correct message', function() {
@@ -133,12 +104,12 @@ wrapper
 
 // it('changes message to thinking', function() {
 //   let wrapper = mount(<EightBall />);
-  
+
 //   wrapper.setState({answers: [
 //     { msg: "It is certain.", color: "green" },
 //     { msg: "It is decidedly so.", color: "green" },
 //   ]});
-  
+
 //   wrapper.find('#EightBall').simulate("click");
 //   expect(wrapper.exists('p')).toEqual(true);
 
@@ -149,12 +120,12 @@ wrapper
 
 // it('works with random messages', function() {
 //   let wrapper = mount(<EightBall />);
-  
+
 //   wrapper.setState({answers: [
 //     { msg: "It is certain.", color: "green" },
 //     { msg: "It is decidedly so.", color: "green" },
 //   ]});
-  
+
 //   wrapper.find('#EightBall').simulate("click");
 //   expect(wrapper.exists('p')).toEqual(true);
 
@@ -167,16 +138,15 @@ wrapper
 
 // it('resets the ball to Question', function() {
 //   let wrapper = mount(<EightBall />);
-  
+
 //   wrapper.setState({answers: [
 //     { msg: "It is certain.", color: "green" },
 //     { msg: "It is decidedly so.", color: "green" },
 //   ]});
-  
+
 //   wrapper.find('#reset').simulate("click");
 //   expect(wrapper.exists('p')).toEqual(true);
 
 //   expect(wrapper.find("#message").html()).toContain("Think of a Question");
 
 // });
-
